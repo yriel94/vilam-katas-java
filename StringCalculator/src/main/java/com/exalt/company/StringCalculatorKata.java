13804bbfc4ecf55cedf86770bee67d2059ed1e2a
vilam <vilam@smile.fr>
2022-09-23 18:09:41 +0200

    Revert "Add implementation of StringCalculator Step 1"

--- StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
+++ StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,20 +1,8 @@
 package com.exalt.company;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class StringCalculatorKata {
 
     int add(String numbers) {
-        if (numbers == null || numbers.isEmpty()) {
-            return 0;
-        }
-
-        String[] separatedNumbers = numbers.split(",");
-        List<String> listOfNumbers = Arrays.asList(separatedNumbers);
-
-        return listOfNumbers.stream()
-                .map(Integer::parseInt)
-                .reduce(0, Integer::sum);
+        throw new UnsupportedOperationException("Please, complete this method with your implementation");
     }
 }
