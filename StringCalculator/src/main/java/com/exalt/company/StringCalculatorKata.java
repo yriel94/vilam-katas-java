ab2c2d114c764db66864201169f9c58f15353b52
vilam <vilam@smile.fr>
2022-09-23 19:40:17 +0200

    Add implementation of StringCalculatorKata Step 4

--- StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
+++ StringCalculator/src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -6,14 +6,27 @@ import java.util.List;
 public class StringCalculatorKata {
 
     int add(String numbers) {
+        String separator = ",\\n";
+        String newSeparator = "";
+
         if (numbers == null || numbers.isEmpty()) {
             return 0;
         }
 
-        String[] separatedNumbers = numbers.split("[,\\n]");
+        if (numbers.length() > 3) {
+            newSeparator = numbers.substring(0, 3);
+
+            if (newSeparator.charAt(0) == '/' && newSeparator.charAt(1) == '/') {
+                separator = separator.concat(newSeparator.substring(2, 3));
+                numbers = numbers.substring(3);
+            }
+        }
+
+        String[] separatedNumbers = numbers.split("[" + separator +"]");
         List<String> listOfNumbers = Arrays.asList(separatedNumbers);
 
         return listOfNumbers.stream()
+                .filter(number -> !number.isEmpty())
                 .map(Integer::parseInt)
                 .reduce(0, Integer::sum);
     }
