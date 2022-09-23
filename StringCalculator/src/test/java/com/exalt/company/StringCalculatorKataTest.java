bd2ae00b2aa5a12ed140b96f2cde30306e72e285
vilam <vilam@smile.fr>
2022-09-23 18:20:57 +0200

    Add unit test for Step 2 StringCalculatorKata

--- StringCalculator/src/test/java/com/exalt/company/StringCalculatorKataTest.java
+++ StringCalculator/src/test/java/com/exalt/company/StringCalculatorKataTest.java
@@ -28,4 +28,16 @@ class StringCalculatorKataTest {
         StringCalculatorKata stringCalculator = new StringCalculatorKata();
         assertEquals(3, stringCalculator.add("1,2"));
     }
+
+    @Test
+    void numbersContainsFourNumberShouldReturnTheSumOfNumbers() {
+        StringCalculatorKata stringCalculator = new StringCalculatorKata();
+        assertEquals(17, stringCalculator.add("1,2,5,9"));
+    }
+
+    @Test
+    void numbersContainsTenNumberShouldReturnTheSumOfNumbers() {
+        StringCalculatorKata stringCalculator = new StringCalculatorKata();
+        assertEquals(42, stringCalculator.add("1,2,5,9,1,10,11,1,1,1"));
+    }
 }
