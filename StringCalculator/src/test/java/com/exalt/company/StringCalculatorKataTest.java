9f2b4a5c5a5efd6b3c78083060af4a979304bb4d
vilam <vilam@smile.fr>
2022-09-23 18:49:50 +0200

    Add failing test for StringCalculatorKata Step 3

--- StringCalculator/src/test/java/com/exalt/company/StringCalculatorKataTest.java
+++ StringCalculator/src/test/java/com/exalt/company/StringCalculatorKataTest.java
@@ -11,19 +11,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class StringCalculatorKataTest {
     @ParameterizedTest
     @MethodSource
-    void sumNumbersSeparatedByComa(String numbers, int result) {
+    void sumNumbersSeparatedByAnySeparator(String numbers, int result) {
         StringCalculatorKata stringCalculator = new StringCalculatorKata();
         assertEquals(result, stringCalculator.add(numbers));
     }
 
-    private static Stream<Arguments> sumNumbersSeparatedByComa() {
+    private static Stream<Arguments> sumNumbersSeparatedByAnySeparator() {
         return Stream.of(
                 Arguments.of(null, 0),
                 Arguments.of("", 0),
                 Arguments.of("1", 1),
                 Arguments.of("1,2", 3),
                 Arguments.of("1,2,5,9", 17),
-                Arguments.of("1,2,5,9,1,10,11,1,1,1", 42));
+                Arguments.of("1,2,5,9,1,10,11,1,1,1", 42),
+                Arguments.of("1\n2,3", 6));
     }
 
 }
